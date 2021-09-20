0e05ef51572cf0c5e0bc6ed52af0dd221900c05b
Jakub Světlík <svetlja1@uhk.cz>
2021-09-20 12:34:51 +0200

    getry setry

---
+++ src/cz/uhk/algds/Main.java
@@ -0,0 +1,7 @@
+package cz.uhk.algds;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/cz/uhk/algds/Models/PizzaMenuItem.java
@@ -0,0 +1,37 @@
+package cz.uhk.algds.Models;
+
+public class PizzaMenuItem {
+    private String name;
+    private String description;
+    private int price;
+
+    public PizzaMenuItem(String name, String description, int price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
